6de9a26658bd418e69091c6eeaa2123abe5085f0
Yuvraj883 <kingyuvi88@gmail.com>
2021-12-03 12:48:55 +0530

    Creating the Vue app

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product : 'Socks',
+           description:'These are a great pair of socks. Available in two colors green and blue'
+        }
+    }
+})
